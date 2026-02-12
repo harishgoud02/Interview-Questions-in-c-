7c2a03b616090684e10ac606c2450ea1fcf9b338
harishgoud02 <vattikutiharish02@gmail.com>
2026-02-12 14:57:22 +0530

    Add files via upload

---
+++ marrige. c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("program starts HARISH:\n");
+    int a;
+    printf("enter the value in a:\n") ;
+    scanf("%d",&a);
+    if(a>=23)
+    {
+    printf("%d is eligible for marriage \n",a);
+    }
+    else
+    {
+    printf("%d is not eligible for marriage\n",a);
+    }
+    printf("program ends");
+    }
